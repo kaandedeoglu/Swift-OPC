54e5f7854d6447636faed546ba3cef00b03e823b
Kaan Dedeoglu <kaandedeoglu@me.com>
2016-05-25 18:41:16 +0300

    Update Package name to OPC from Swift-OPC, which would needed to be imported as Swift_OPC in code.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swift-OPC", 
+    name: "OPC", 
     dependencies: [
     	.Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 5)
     ]
