5c74a4e10de6e228ea37dabf0f5ce75b96a47de1
Kaan Dedeoglu <kaandedeoglu@me.com>
2016-05-25 18:18:53 +0300

    Package.swift changes

--- Package.swift
+++ Package.swift
@@ -4,7 +4,5 @@ let package = Package(
     name: "Swift-OPC", 
     dependencies: [
     	.Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 5)
-    ],
-    targets: [Target(name: "OPC")],
-    exclude: ["Swift-OPC.xcodeproj", "README.md","Tests"]
+    ]
 )
